e1dfda099dedf39ce3cfafaf729f105116074d3d
Jakob Olsson <53495529+maskenjesper@users.noreply.github.com>
2023-01-26 14:48:22 +0100

    fix: export axios

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,11 @@ import Axios from 'axios';
 
 let baseURL;
 if (process.env.NODE_ENV === 'development') {
-  baseURL = 'http://localhost:5001';
+  baseURL = 'http://localhost:5001/';
 } else if (process.env.NODE_ENV === 'production') {
-  baseURL = 'https://iv1201-backend.onrender.com';
+  baseURL = 'https://iv1201-backend.onrender.com/';
 }
+
+export const axios = Axios.create({
+  baseURL: baseURL,
+});
