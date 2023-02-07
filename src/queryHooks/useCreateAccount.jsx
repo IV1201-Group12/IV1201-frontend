fca46538f39c855b20c9dac9b552693162ae0683
Abhinav Kalra <abhinavkalra2002@gmail.com>
2023-02-07 15:33:49 +0100

    fix: use useMutation correctly

--- src/queryHooks/useCreateAccount.jsx
+++ src/queryHooks/useCreateAccount.jsx
@@ -1,10 +1,14 @@
-import { useMutation } from '@tanstack/react-query';
-import { createNewAccount } from '../api/createAccount';
-export function useCreateAccount(applicant) {
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { signUpUser } from '../api/auth';
+export function useCreateAccount() {
+  const queryClient = useQueryClient();
   return useMutation({
-    mutationFn: () => createNewAccount(applicant),
-    onSuccess: () => {
-      console.log('Success');
+    mutationFn: async (user) => {
+      const result = await signUpUser(user);
+      return result;
+    },
+    onSuccess: (result) => {
+      queryClient.setQueryData(['statusCode'], result.status);
     },
   });
 }
