ab572f395addb7aafd01dd468ed638e728030d08
Abhinav Kalra <abhinavkalra2002@gmail.com>
2023-02-07 17:31:03 +0100

    refactor: change to RegistrationForm

--- src/components/RegistrationForm.jsx
+++ src/components/RegistrationForm.jsx
@@ -1,98 +1,126 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
+
 import { useRegister } from '../queryHooks/useRegister';
 
-const RegistrationForm = () => {
+export default function RegistrationForm() {
+  const { isError, error, isLoading, mutate, isSuccess } = useRegister();
   const [firstname, setFirstname] = useState('');
   const [lastname, setLastname] = useState('');
   const [email, setEmail] = useState('');
   const [pnr, setPnr] = useState('');
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const register = useRegister();
 
+  const submit = async () => {
+    const applicant = {
+      firstname,
+      lastname,
+      email,
+      pnr,
+      username,
+      password,
+      role: 'applicant',
+    };
+    mutate(applicant);
+  };
+
+  const onChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    if (name === 'firstname') {
+      setFirstname(value);
+    }
+
+    if (name === 'lastname') {
+      setLastname(value);
+    }
+
+    if (name === 'email') {
+      setEmail(value);
+    }
+
+    if (name === 'pnr') {
+      setPnr(value);
+    }
+
+    if (name === 'username') {
+      setUsername(value);
+    }
+    if (name === 'password') {
+      setPassword(value);
+    }
+  };
   return (
     <div>
-      <label>Firstname </label>
-      <input
-        type="text"
-        id="firstname-input"
-        placeholder="Firstname"
-        value={firstname}
-        onChange={(event) => setFirstname(event.target.value)}
-      />
-      <br />
-      <br />
-      <label>Lastname </label>
-      <input
-        type="text"
-        id="lastname-input"
-        placeholder="Lastname"
-        value={lastname}
-        onChange={(event) => setLastname(event.target.value)}
-      />
-      <br />
-      <br />
-      <label>Email </label>
-      <input
-        type="text"
-        id="email-input"
-        placeholder="Email"
-        value={email}
-        onChange={(event) => setEmail(event.target.value)}
-      />
-      <br />
-      <br />
-      <label>Person Number </label>
-      <input
-        type="text"
-        id="pnr-input"
-        placeholder="Person Number"
-        value={pnr}
-        onChange={(event) => setPnr(event.target.value)}
-      />
-      <br />
-      <br />
-      <label>Username </label>
-      <input
-        type="text"
-        id="username-input"
-        placeholder="Username"
-        value={username}
-        onChange={(event) => setUsername(event.target.value)}
-      />
-      <br />
-      <br />
-      <label>Password </label>
-      <input
-        type="password"
-        id="password-input"
-        placeholder="Password"
-        value={password}
-        onChange={(event) => setPassword(event.target.value)}
-      />
-      <br />
-      <br />
+      {isLoading ? <h1>Loading...</h1> : null}
+      {isSuccess ? (
+        <h1 style={{ color: 'green' }}>New account created</h1>
+      ) : null}
+      {isError ? (
+        <h1 style={{ color: 'red' }}>{error.response.data} </h1>
+      ) : null}
+      <form>
+        <label htmlFor="firstname">First Name</label>
+        <input
+          id="firstname"
+          name="firstname"
+          type="text"
+          value={firstname}
+          onChange={onChange}
+        />
+        <label htmlFor="lastname">Last Name</label>
+        <input
+          id="lastname"
+          name="lastname"
+          type="text"
+          value={lastname}
+          onChange={onChange}
+        />
+        <label htmlFor="email">Email Address</label>
+        <input
+          id="email"
+          name="email"
+          type="email"
+          value={email}
+          onChange={onChange}
+        />
+        <label htmlFor="pnr">Person Number</label>
+        <input
+          id="pnr"
+          name="pnr"
+          type="number"
+          value={pnr}
+          placeholder="YYYYMMDDXXXX"
+          onChange={onChange}
+        />
+        <label htmlFor="username">Username</label>
+        <input
+          id="username"
+          name="username"
+          type="text"
+          value={username}
+          onChange={onChange}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          id="password"
+          name="password"
+          type="password"
+          value={password}
+          onChange={onChange}
+        />
+      </form>
       <button
-        type="submit"
+        type="button"
         id="button-input"
         disabled={
           !(firstname && lastname && email && pnr && username && password)
         }
-        onClick={(e) =>
-          register.mutate({
-            firstname,
-            lastname,
-            email,
-            pnr,
-            username,
-            password,
-          })
-        }
+        onClick={submit}
       >
-        Create Account
+        Create account
       </button>
     </div>
   );
-};
-
-export default RegistrationForm;
+}
