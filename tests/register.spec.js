604c7ef68b3b40b9ab26c90832544402289b950d
EdvAlvaKTH <ealvaeus@kth.se>
2023-02-15 17:08:26 +0100

    chore: test setup

--- tests/register.spec.js
+++ tests/register.spec.js
@@ -6,19 +6,18 @@ test.beforeEach(async ({ page }) => {
 
 test.afterEach(async () => {});
 
-// test('test', async ({ page }) => {
-//   await page.fill('input[name=firstname]', 'John');
-//   await page.fill('input[name=lastname]', 'Doe');
-//   await page.fill('input[name=email]', 'john.doe@example.com');
-//   await page.fill('input[name=pnr]', '198012312348');
-//   await page.fill('input[name=username]', 'johndoe');
-//   await page.fill('input[name=password]', 'secretpassword');
-//   await page.click('button');
-//   await page.waitForSelector('h1[style="color: green;"]');
-
-//   const successMessage = await page.$eval(
-//     'h1[style="color: green;"]',
-//     (el) => el.textContent,
-//   );
-//   expect(successMessage).toBe('New account created');
-// });
+test('test', async ({ page }) => {
+  //   await page.fill('input[name=firstname]', 'John');
+  //   await page.fill('input[name=lastname]', 'Doe');
+  //   await page.fill('input[name=email]', 'john.doe@example.com');
+  //   await page.fill('input[name=pnr]', '198012312348');
+  //   await page.fill('input[name=username]', 'johndoe');
+  //   await page.fill('input[name=password]', 'secretpassword');
+  //   await page.click('button');
+  //   await page.waitForSelector('h1[style="color: green;"]');
+  //   const successMessage = await page.$eval(
+  //     'h1[style="color: green;"]',
+  //     (el) => el.textContent,
+  //   );
+  //   expect(successMessage).toBe('New account created');
+});
