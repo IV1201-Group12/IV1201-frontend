17af6a0f350c65a728ccc00ca30b6d2e13848ce8
Jakob Olsson <53495529+maskenjesper@users.noreply.github.com>
2023-02-16 01:12:34 +0100

    chore:fix

--- src/config/api-config.js
+++ src/config/api-config.js
@@ -1,6 +1,8 @@
 let baseURL;
 if (process.env.NODE_ENV === 'development') {
-  baseURL = `http://127.0.0.1:${import.meta.env.PORT}`;
+  baseURL = `http://${import.meta.env.REACT_APP_BACKEND_HOST}:${
+    import.meta.env.REACT_APP_BACKEND_PORT
+  }`;
 } else if (process.env.NODE_ENV === 'production') {
   baseURL = 'https://iv1201-backend.onrender.com';
 }
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,10 +1,5 @@
 const LandingPage = () => {
-  return (
-    <>
-      This is a landing page test test test test test test{' '}
-      {import.meta.env.REACT_APP_PORT}
-    </>
-  );
+  return <>This is a landing page test test test test test test</>;
 };
 
 export default LandingPage;
