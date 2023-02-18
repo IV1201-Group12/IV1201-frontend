f2834e3a8168f7647ff108942e8d14d15209e735
EdvAlvaKTH <ealvaeus@kth.se>
2023-02-18 15:16:56 +0100

    feat: log out client on 401 response

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,61 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ApplicationsPage from './pages/ApplicationsPage';
 import RegisterPage from './pages/RegisterPage';
 import LoginPage from './pages/LoginPage';
 import ApplicationPage from './pages/ApplicationPage';
 import PrivateRoute from './components/PrivateRoute';
 import LandingPage from './pages/LandingPage';
+import {
+  QueryCache,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import useAuth from './context/AuthContext';
+import { logoutUser } from './api/auth';
 
 function App() {
+  const { removeCurrentUser } = useAuth();
+  const queryClient = new QueryClient({
+    queryCache: new QueryCache({
+      onError: async (error) => {
+        if (error?.response?.status === 401) {
+          removeCurrentUser();
+          await logoutUser();
+        }
+      },
+    }),
+  });
+
   return (
-    <div className="container">
-      <Router>
-        <Routes>
-          <Route exact path="/" element={<LandingPage />} />
-          <Route
-            path="/applications"
-            element={
-              <PrivateRoute roles={['recruiter']}>
-                <ApplicationsPage />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/applications/:id"
-            element={
-              <PrivateRoute roles={['recruiter']}>
-                <ApplicationPage />
-              </PrivateRoute>
-            }
-          />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/login" element={<LoginPage />} />
-        </Routes>
-      </Router>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="container">
+        <Router>
+          <Routes>
+            <Route exact path="/" element={<LandingPage />} />
+            <Route
+              path="/applications"
+              element={
+                <PrivateRoute roles={['recruiter']}>
+                  <ApplicationsPage />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/applications/:id"
+              element={
+                <PrivateRoute roles={['recruiter']}>
+                  <ApplicationPage />
+                </PrivateRoute>
+              }
+            />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/login" element={<LoginPage />} />
+          </Routes>
+        </Router>
+      </div>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
 
